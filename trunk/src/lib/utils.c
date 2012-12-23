36942f3757d42be899d125a44752b4dc6ac07466
alexandre.dussart@gmail.com <alexandre.dussart@gmail.com@320e8099-a072-2a08-7711-9ec5fe860cba>
2012-12-23 17:07:36 +0000

    ADD:add location use of frame arround location image+select file button. ADD:add location use of frame to display entry request in a nice way. ADD:add location use a larger elm_map. ADD:add location got reduced buttons(ok,close). CLEAN:add location select file button label 'select' is 'open' now. CLEAN:remove devel printf.

--- trunk/src/lib/utils.c
+++ trunk/src/lib/utils.c
@@ -18,87 +18,78 @@
  * along with EDAMS. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-
-
 #include <ctype.h>
 
 #include "libedams.h"
 #include "gettext.h"
 
-
 //Keep aspect ratio.
-void
-evas_object_image_scale(Evas_Object *obj, int width, int height)
+void evas_object_image_scale (Evas_Object * obj, int width, int height)
 {
-	int w3, h3;
-	int w2, h2;
+   int w3, h3;
+   int w2, h2;
 
-	evas_object_image_size_get(obj, &w2, &h2);
+   evas_object_image_size_get (obj, &w2, &h2);
 
-	w3 = width;
-	h3 = ((double)w3 * h2)/ (double)w2;
+   w3 = width;
+   h3 = ((double) w3 * h2) / (double) w2;
 
+   if (h3 > height)
+   {
+      h3 = height;
+      w3 = ((double) h3 * w2) / (double) h2;
+   }
 
-        if(h3 > height)
-        {
-                h3 = height;
-                w3 = ((double)h3 * w2) / (double)h2;
-        }
-
-  	evas_object_image_fill_set(obj, 0, 0, w3, h3);
-	evas_object_resize(obj, w3, h3);
+   evas_object_image_fill_set (obj, 0, 0, w3, h3);
+   evas_object_resize (obj, w3, h3);
 }
 
-
 //
 //
 //
-const char *user_home_get(void)
+const char *user_home_get (void)
 {
-    return(getenv ("HOME"));
+   return (getenv ("HOME"));
 }
-
-
 
 //
 //Close a window by deleting evas object passing arg.
 //
 void
-window_clicked_close_cb(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+window_clicked_close_cb (void *data, Evas_Object * obj __UNUSED__,
+                         void *event_info __UNUSED__)
 {
-	evas_object_del(data);
+   evas_object_del (data);
 }
-
-
 
 //
 //Show a window message to inform user about something.
 //
-void
-msgbox(const char *msg)
+void msgbox (const char *msg)
 {
    Evas_Object *win, *popup, *ic;
    Evas_Object *bt;
 
-   win = elm_win_util_standard_add("confirmation", _("Confirmation"));
-   elm_win_autodel_set(win, EINA_TRUE);
-   elm_win_center(win, EINA_TRUE, EINA_TRUE);
-   evas_object_show(win);
-   evas_object_resize(win, 480, 400);
+   win = elm_win_util_standard_add ("confirmation", _("Confirmation"));
+   elm_win_autodel_set (win, EINA_TRUE);
+   elm_win_center (win, EINA_TRUE, EINA_TRUE);
+   evas_object_show (win);
+   evas_object_resize (win, 480, 400);
 
-   popup = elm_popup_add(win);
-   evas_object_size_hint_weight_set(popup, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   elm_object_text_set(popup, msg);
+   popup = elm_popup_add (win);
+   evas_object_size_hint_weight_set (popup, EVAS_HINT_EXPAND,
+                                     EVAS_HINT_EXPAND);
+   elm_object_text_set (popup, msg);
 
-   bt = elm_button_add(popup);
-   elm_object_text_set(bt, _("Ok"));
-   elm_object_part_content_set(popup, "button1", bt);
-   ic = elm_icon_add(popup);
-   elm_icon_order_lookup_set(ic, ELM_ICON_LOOKUP_FDO_THEME);
-   elm_icon_standard_set(ic, "apply-window");
-   elm_object_part_content_set(bt, "icon", ic);
-	evas_object_smart_callback_add(bt, "clicked", window_clicked_close_cb, win);
+   bt = elm_button_add (popup);
+   elm_object_text_set (bt, _("Ok"));
+   elm_object_part_content_set (popup, "button1", bt);
+   ic = elm_icon_add (popup);
+   elm_icon_order_lookup_set (ic, ELM_ICON_LOOKUP_FDO_THEME);
+   elm_icon_standard_set (ic, "apply-window");
+   elm_object_part_content_set (bt, "icon", ic);
+   evas_object_smart_callback_add (bt, "clicked", window_clicked_close_cb,
+                                   win);
 
-	evas_object_show(popup);
+   evas_object_show (popup);
 }
