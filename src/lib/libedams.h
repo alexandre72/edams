3c32ba04eb996b08ec6b6b99503ee9ec6fec3251
alexandre.dussart <alexandre.dussart@gmail.com>
2013-01-10 15:55:59 +0000

    CLEAN:need to be removed, obsolete all is in utils.h and utils.c now.

--- src/lib/libedams.h
+++ src/lib/libedams.h
@@ -17,47 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with EDAMS. If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef __LIBEDAMS_H__
-#define __LIBEDAMS_H__
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include <Elementary.h>
-
-#ifdef HAVE_EFREET
-#include <Efreet.h>
-#endif
-
-#include "gettext.h"
-
-#define DIR_SEPARATOR '/'
-#define DIR_SEPARATOR_S "/"
-
-#define _(String) gettext (String)
-#define gettext_noop(String) String
-#define N_(String) gettext_noop (String)
-
-#define __UNUSED__ __attribute__((unused))
-
-#define FREE(ptr)  do { _free( #ptr, __FILE__, __LINE__, (ptr)); (ptr) = NULL; } while (0)
-
-inline void
-_free(const char * var, const char * filename, unsigned long line, void *ptr)
-{
-    //++free_count;
-    //INF(_("Variable %s (%10p) at %s:%lu"), var, ptr, filename, line);
-    if (ptr)
-    {
-        free(ptr);
-        ptr = NULL;
-    }
-    else
-    {
-        fprintf(stderr, _("Caught attempt to free NULL pointer variable %s at %s:%lu!"), var, filename, line);
-    }
-}//_free
 
 
 #endif /* __LIBEDAMS_H__ */
